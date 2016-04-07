7353bab31a151e5629b7d9e9c5fe7336b6430686
Ioma Taani <iomataani@users.noreply.github.com>
2016-04-07 08:52:35 +0200

    il bot nasce!

---
+++ psyfuck.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+#
+#psyfuck bot, la mascotte spixellata
+
+require 'telegram/bot'
+require 'colorize'
+require 'logger'
+
+TOKEN = File.open("api.txt", "r").readlines
+
+
