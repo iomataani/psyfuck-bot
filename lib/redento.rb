e0cea65558835793f8ee6edb6eb1231582f14f81
Ioma Taani <iomataani@users.noreply.github.com>
2016-05-04 10:08:00 +0200

    redento is real

--- lib/redento.rb
+++ lib/redento.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 #encoding: utf-8
 
-require 'open-uri'
+require 'net/http'
 
+def redento(rom)
+  return "http://www.emuparadise.me/roms/search.php?query=#{rom.gsub(" ", "%20")}"
+end
 
